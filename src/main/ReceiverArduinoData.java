5b5b29ae30375fbdc1ae9132c80c732c66659e28
ilhamrahman <30919695+ilhamrahman@users.noreply.github.com>
2019-10-15 15:50:25 -0400

    Send data to Camera initialized

--- src/main/ReceiverArduinoData.java
+++ src/main/ReceiverArduinoData.java
@@ -21,9 +21,9 @@ public class ReceiverArduinoData {
 		System.out.println("z = " + entranceBoundary[0] + "x+" + entranceBoundary[1]+ "y+" + entranceBoundary[2]);
 		
 		double[] angles = camera.angles();
-		System.out.println("Angle on x-y Plane (in radians) = " + angles[0]);
-		System.out.println("Angle on x-z Plane (in radians) = " + angles[1]);
-		System.out.println("Angle on y-z Plane (in radians) = " + angles[2]);
+		System.out.println("Angle on x axis (in radians) = " + angles[0]);
+		System.out.println("Angle on y axis (in radians) = " + angles[1]);
+		System.out.println("Angle on z axis (in radians) = " + angles[2]);
 		
 		
 	      // Check the arguments
---
+++ src/main/SendToCamera.java
@@ -0,0 +1,21 @@
+package main;
+
+import org.json.*;
+
+public class SendToCamera {
+
+public JSONObject params;
+	
+	public SendToCamera() {
+		
+	}
+
+	public JSONObject SendThis(double xAngle, double yAngle, double zAngle) throws JSONException {
+		//Get the information that will go through the sender
+		params = new JSONObject();
+		params.put("Angle to be moved on x-axis", xAngle);
+		params.put("Angle to be moved on y-axis", yAngle);
+		params.put("Angle to be moved on z-axis", zAngle);
+		return params;
+		}
+}
