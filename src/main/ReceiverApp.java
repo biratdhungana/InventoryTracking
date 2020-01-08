7701686371d6e8565d3d49e886c0180c6ea209f7
ilhamrahman <ilhamrahman@cmail.carleton.ca>
2020-01-08 19:07:05 +0000

    receive location data SUCCESS

--- src/main/ReceiverApp.java
+++ src/main/ReceiverApp.java
@@ -12,21 +12,21 @@ public class ReceiverApp {
 	
 	public void receive() throws Exception {
 		
-		ServerSocket sersock = new ServerSocket(6001);
+            while (true){
+	      ServerSocket sersock = new ServerSocket(6001);
 	      System.out.println("Server  ready to receive data from App");
 	      Socket sock = sersock.accept( );                          
 	      InputStream istream = sock.getInputStream();
 	      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 	 
 	      String receiveMessage;               
-	      while(true)
-	      {
 	        //receiveMessage = receiveRead.readLine();
 	        if((receiveMessage = receiveRead.readLine()) != null)  
 	        {
 	           System.out.println("Data received from App: " + receiveMessage);         
+		   sersock.close();
 	        }         
 	      
-	      }               
+	     }               
 	}
 }
