ade825956a52622eee0f2e89f71b5ea7f58f718c
ilhamrahman <30919695+ilhamrahman@users.noreply.github.com>
2020-02-11 12:49:37 -0500

    Added users table

--- Database/inventorytracking_users.sql
+++ Database/inventorytracking_users.sql
@@ -16,29 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `tags`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `tags`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tags` (
-  `idTags` varchar(45) NOT NULL,
-  `xCoordinate` double NOT NULL,
-  `yCoordinate` double NOT NULL,
-  `zCoordinate` double NOT NULL,
-  PRIMARY KEY (`idTags`)
+CREATE TABLE `users` (
+  `userid` int NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tags`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `tags` WRITE;
-/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-INSERT INTO `tags` VALUES ('1234',40.5,54.3,60.4),('1235',66.5,47.3,73.4),('1236',43.5,57.3,64.4),('1237',4332.5,5656.3,632.4),('1248',4223.5,574.3,664.4);
-/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-11 12:48:18
+-- Dump completed on 2020-02-11 12:48:17
